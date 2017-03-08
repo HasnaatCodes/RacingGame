caa1795ff90e69aebbbfbb6412e1536962617ac5
Akhtar <akhtarhn@lancs.local>
2017-03-08 15:35:23 +0000

    Adjusted the size of the Score label and positioned it

--- RacingGame.java
+++ RacingGame.java
@@ -6,19 +6,19 @@ public class RacingGame
 	private JFrame content = new JFrame();
 	private Racer player = new Racer();
 	private JPanel mainPanel = new JPanel();
-	private JLabel scoreLabel = new JLabel("Score");
+	private JLabel scoreLabel = new JLabel("Score: ");
 	private int score = 0;
 
 
 	public RacingGame()
 	{
-		
+		mainPanel.setLayout(new BorderLayout());
 		content.setTitle("Racing Game");
 		content.setSize(800, 700);
 		mainPanel.add(player.getPanel());
-		scoreLabel.setPreferredSize(new Dimension(50, 50));
+		scoreLabel.setPreferredSize(new Dimension(20, 20));
 		scoreLabel.setHorizontalAlignment(JLabel.LEFT);
-		mainPanel.add(scoreLabel);
+		mainPanel.add("North", scoreLabel);
 		content.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		content.setContentPane(mainPanel);
 		content.setVisible(true);
